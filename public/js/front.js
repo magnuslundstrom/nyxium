55496606d8df3c80ac7ea38d5da199a23e411aef
magnussony <magnuslundstromdk@gmail.com>
2020-02-13 16:30:42 +0100

    nyxium project first init

---
+++ public/js/front.js
@@ -0,0 +1,106 @@
+
+
+// QUERY SELECTORS ###
+
+const nav = document.querySelector('nav');
+const burger = document.querySelector('.burger')
+const bars = document.querySelector('.bars')
+const frontPageH1 = document.querySelector('.fph1')
+const typeEffect = document.querySelector('.effect')
+
+// Testimonials
+const testimonials = document.querySelector('.testimonials')
+const testimonialGrids = document.querySelectorAll('.testimonials_grid')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (testimonials) {
+    const options = {
+        threshold: 0.3
+    }
+    const testimonialObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                console.log(entry.isIntersecting)
+                let duration = 400;
+                testimonialGrids.forEach(el => {
+                    setTimeout(() => {
+
+                        el.classList.add('testimonials-pop')
+
+                    }, duration)
+                    duration = duration + 400
+                })
+            }
+        })
+    }, options)
+    testimonialObserver.observe(testimonials)
+}
+// Menu effect
+const navToggle = () => {
+    nav.classList.toggle('displaynav')
+    bars.classList.toggle('cross')
+}
+burger.addEventListener('click', navToggle)
+
+// Frontpage Typeeffect
+const typescript = () => {
+    var typewriter = new Typewriter(typeEffect, {
+        loop: true
+    });
+
+    typewriter.typeString('salg')
+        .pauseFor(2000)
+        .deleteChars(4)
+        .typeString('indkøb')
+        .pauseFor(2500)
+        .deleteChars(6)
+        .typeString('din virksomhed')
+        .pauseFor(2000)
+        .start();
+}
+if (frontPageH1) {
+    typescript()
+}
---
+++ src/app.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const http = require('http')
+// const reload = require('reload')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+
+// Paths
+
+const publicDirPath = path.join(__dirname, '../public/')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Handlebar setup, the engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+// Static Dir
+app.use(express.static(publicDirPath))
+
+
+
+
+
+
+
+
+app.get('', (req, res) => {
+
+
+    res.render('index', {
+        metaTitle: 'Front page title',
+        metaDescription: 'Welcome to Nyxium front page'
+    })
+})
+
+const server = http.createServer(app)
+server.listen(3000, () => console.log('server started!'))
+
+
+
+// reload(app) --- dev purpose
+
