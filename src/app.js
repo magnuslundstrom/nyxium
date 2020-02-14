9683928918d5b2446e30988b2caa3df48d67c597
magnussony <magnuslundstromdk@gmail.com>
2020-02-14 15:06:15 +0100

    a couple of new pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const http = require('http')
-// const reload = require('reload')
+const reload = require('reload')
 const express = require('express')
 const hbs = require('hbs')
 
@@ -28,20 +28,33 @@ app.use(express.static(publicDirPath))
 
 
 
-
+// Forside
 app.get('', (req, res) => {
-
-
     res.render('index', {
         metaTitle: 'Front page title',
-        metaDescription: 'Welcome to Nyxium front page'
+        metaDescription: 'Welcome to Nyxium front page',
+        coverBg: 'fpCover'
+    })
+})
+
+// Integrationer hovedside
+app.get('/integrationer', (req, res) => {
+    res.render('integrationer', {
+        metaTitle: 'Integrationer',
+        metaDescription: 'Integrationer til Nyxium',
+        coverBg: 'intCover'
+    })
+})
+
+// Integrationer Dinero side
+app.get('/integrationer/dinero', (req, res) => {
+    res.render('integration-emne', {
+        metaTitle: 'Dinero integration',
+        metaDescription: 'Dinero integration',
+        coverBg: 'intDineroCover'
     })
 })
 
 const server = http.createServer(app)
 server.listen(port, () => console.log('server started!'))
-
-
-
-// reload(app) --- dev purpose
-
+reload(app) 
